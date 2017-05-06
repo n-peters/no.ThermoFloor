bb2e8e561be6f8c45434cd35d50ca888ccac0739
dT <dTNL.Homey@gmail.com>
2017-05-06 23:13:50 +0200

    Adding capabilities to app and driver
    measure_temperature
    target_temperature
    thermofloor_mode

--- drivers/TF_Thermostat/driver.js
+++ drivers/TF_Thermostat/driver.js
@@ -7,7 +7,101 @@ const ZwaveDriver = require('homey-zwavedriver');
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
 	debug: true,
-	capabilities: {},
+	capabilities: {
+		measure_temperature: {
+			command_class: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
+			command_get: 'SENSOR_MULTILEVEL_GET',
+			command_report: 'SENSOR_MULTILEVEL_REPORT',
+			command_report_parser: report => {
+				if (!report) return null;
+				if (report['Sensor Type'] === 'Temperature (version 1)') return report['Sensor Value (Parsed)'];
+				return null;
+			},
+		},
+		target_temperature: {
+			command_class: 'COMMAND_CLASS_THERMOSTAT_SETPOINT',
+			command_get: 'THERMOSTAT_SETPOINT_GET',
+			command_get_parser: node => {
+				let mode = 'Heating 1';
+				if (node && typeof node.state.eurotronic_mode !== 'undefined' && node.state.eurotronic_mode === 'Energy Save Heat') {
+					mode = 'Energy Save Heating 2';
+				}
+				return {
+					Level: {
+						'Setpoint Type': mode,
+					},
+				};
+			},
+			command_set: 'THERMOSTAT_SETPOINT_SET',
+			command_set_parser: value => {
+				// Create 2 byte buffer of value, with value rounded to xx.5
+				if (!value) value = 18;
+				let newTemp = new Buffer(2);
+				newTemp.writeUIntBE((value * 2).toFixed() / 2 * 10, 0, 2);
+
+				return {
+					Level: {
+						'Setpoint Type': 'Heating 1'
+					},
+					Level2: {
+						Precision: 1, // Number has one decimal
+						Scale: 0, // No scale used
+						Size: 2, // Value = 2 Bytes
+					},
+					Value: newTemp,
+				};
+			},
+			command_report: 'THERMOSTAT_SETPOINT_REPORT',
+			command_report_parser: report => {
+				if (report &&
+					report.hasOwnProperty('Value') &&
+					report.hasOwnProperty('Level2') &&
+					typeof report.Level2.Precision === 'number' &&
+					typeof report.Level2.Size === 'number') {
+
+					let targetValue;
+					try {
+						targetValue = report.Value.readUIntBE(0, report.Level2.Size);
+					}
+					catch (err) {
+						return null;
+					}
+					if (typeof targetValue === 'number') return targetValue / Math.pow(10, report.Level2.Precision);
+					return null;
+				}
+				return null;
+			},
+		},
+		thermofloor_mode: {
+			command_class: 'COMMAND_CLASS_THERMOSTAT_MODE',
+			command_get: 'THERMOSTAT_MODE_GET',
+			command_set: 'THERMOSTAT_MODE_SET',
+			command_set_parser: value => ({
+				Level: {
+					'No of Manufacturer Data fields': 0,
+					Mode: value,
+				},
+				'Manufacturer Data': new Buffer([0]),
+			}),
+			command_report: 'THERMOSTAT_MODE_REPORT',
+			command_report_parser: (report, node) => {
+				if (!report) return null;
+				if (report.hasOwnProperty('Level') && report.Level.hasOwnProperty('Mode')) {
+					if (node) {
+						Homey.manager('flow').triggerDevice('thermofloor_mode_changed', {
+							mode: report.Level.Mode,
+							mode_name: __("mode." + report.Level.Mode)
+						}, null, node.device_data);
+						Homey.manager('flow').triggerDevice('thermofloor_mode_changed_to', null, {
+							mode: report.Level.Mode
+						}, node.device_data);
+					}
+					return report.Level.Mode;
+				}
+				return null;
+			},
+		}
+	},
 	settings: {
 		operation_mode: {
 			index: 1,
@@ -62,4 +156,12 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			size: 2,
 		},
 	},
+});
+
+Homey.manager('flow').on('trigger.thermofloor_mode_changed_to', (callback, args, state) => {
+	if (!args) return callback('arguments_error', false);
+	else if (!state) return callback('state_error', false);
+
+	else if (typeof args.mode !== 'undefined' && typeof state.mode !== 'undefined' && args.mode === state.mode) return callback(null, true);
+	else return callback('unknown_error', false);
 });
